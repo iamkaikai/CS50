2610de99528d8e8c2cd23f14a63b0f6db8079c80
YENKAI HUANG <yenkai.huang.gr@dartmouth.edu>
2023-01-23 16:55:29 -0500

    add init files

---
+++ module3/list.c
@@ -0,0 +1,15 @@
+/* list.c --- 
+ * 
+ * 
+ * Author: YENKAI HUANG, HONGKE (LUIS) LU, ERIN
+ * Created: Mon Jan 23 14:24:41 2023 (-0500)
+ * Version: 
+ * 
+ * Description: Implementation of the linked list
+ * 
+ */
+
+
+# include <stdio.h>
+# include <stdlib.h>
+
