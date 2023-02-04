eaf8c06b2a66085a58bd993fc71d05e80a950737
YENKAI HUANG <yenkai.huang.gr@dartmouth.edu>
2023-02-03 22:56:39 -0500

    initializing files in step1

---
+++ module4/crawler/crawler.c
@@ -0,0 +1,19 @@
+/* crawler.c --- 
+1;95;0c * 
+ * 
+ * Author: YENKAI HUANG, HONGKE (LUIS) LU, ERIN
+ * Created: Fri Feb  3 22:15:27 2023 (-0500)
+ * Version: 1.0
+ * 
+ * Description: tiny search engine in module 4, grounp 2 in W23 CS50 
+ * 
+ */
+
+# include <stdio.h>
+# include <stdlib.h>   
+
+
+int main(void){
+	printf("Hello\n");
+	exit(EXIT_SUCCESS);
+}
