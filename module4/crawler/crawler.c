3a70a124a72dbc179dce1f47a1651832c1bf3766
YENKAI HUANG <yenkai.huang.gr@dartmouth.edu>
2023-02-04 17:56:18 -0500

    step2: fetch a single page

--- module4/crawler/crawler.c
+++ module4/crawler/crawler.c
@@ -11,9 +11,42 @@
 
 # include <stdio.h>
 # include <stdlib.h>   
-
+# include <webpage.h>
 
 int main(void){
+
 	printf("Hello\n");
+	char seed[] = "https://thayer.github.io/engs50/";
+	int depth = 0;
+	webpage_t *page;
+				
+	page = webpage_new(seed, depth, NULL);	
+
+	if(page == NULL){
+		printf("webpage_new error!\n");
+	}else{
+		printf("webpage_new succeed!\n");
+
+		if(webpage_fetch(page)){
+			printf("webpage_fetch succeed!\n");
+
+			int pos = 0;
+			char *result;
+			while((pos = webpage_getNextURL(page, pos, &result)) > 0){
+				if(IsInternalURL(result)){
+					printf("Found URL (internal): %s\n",result);
+				}else{
+					printf("Found URL (external): %s\n",result);
+				}
+				free(result);
+			}
+			
+		}else{
+			exit(EXIT_FAILURE);
+		}
+	}
+
+
+	webpage_delete(page);
 	exit(EXIT_SUCCESS);
 }
