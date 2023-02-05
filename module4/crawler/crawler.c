6379e2a9ffc028c08342e8fc1d822c41373956d6
YENKAI HUANG <yenkai.huang.gr@dartmouth.edu>
2023-02-05 13:47:26 -0500

    crawler.c

--- module4/crawler/crawler.c
+++ module4/crawler/crawler.c
@@ -12,6 +12,14 @@
 # include <stdio.h>
 # include <stdlib.h>   
 # include <webpage.h>
+# include <queue.h>
+
+void print_element(void *p){
+  webpage_t *qp = (webpage_t *) p;
+	char *url = webpage_getURL(qp);
+	printf("URL = %s\n", url);
+	free(url);
+}
 
 int main(void){
 
@@ -19,27 +27,44 @@ int main(void){
 	char seed[] = "https://thayer.github.io/engs50/";
 	int depth = 0;
 	webpage_t *page;
-				
+	void (*fn1)(queue_t *) = print_element;
+	
 	page = webpage_new(seed, depth, NULL);	
 
+	//if seed page fails
 	if(page == NULL){
 		printf("webpage_new error!\n");
+
+	//if seed page initialization succeed, fetch the page
 	}else{
 		printf("webpage_new succeed!\n");
-
+		
 		if(webpage_fetch(page)){
 			printf("webpage_fetch succeed!\n");
 
+			//get all the URL in the seed page
 			int pos = 0;
 			char *result;
+			queue_t *url_queue = qopen();
+			depth +=1;
+			
 			while((pos = webpage_getNextURL(page, pos, &result)) > 0){
+				//check whether the URL is internal
 				if(IsInternalURL(result)){
 					printf("Found URL (internal): %s\n",result);
+					//put URL in to the queue
+					webpage_t *new_page;
+					new_page = webpage_new(result, depth, NULL);
+					qput(url_queue, new_page);
+				 
 				}else{
-					printf("Found URL (external): %s\n",result);
+					printf("Found URL (external): %s\n\n",result);
 				}
 				free(result);
 			}
+				//free the URL for each iteration
+			qapply(url_queue, fn1);
+			qclose(url_queue);
 			
 		}else{
 			exit(EXIT_FAILURE);
