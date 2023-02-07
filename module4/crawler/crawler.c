7b7197f4646c414d5724368f00b540c9f3838fa0
YENKAI HUANG <yenkai.huang.gr@dartmouth.edu>
2023-02-07 18:05:23 -0500

    breadth first search updated

--- module4/crawler/crawler.c
+++ module4/crawler/crawler.c
@@ -20,107 +20,108 @@ void print_element(void *p){
   webpage_t *qp = (webpage_t *) p;
 	char *url = webpage_getURL(qp);
 	if(url != NULL){
-		printf("URL = %s\n", url);
+		printf("%p; URL = %s\n",p , url);
 	}else{
 		printf("queue is empty\n");
 	}
-	free(url);
+	//	free(url);
 }
 
-bool search_url(void *elementp, const void* searchkeyp){
+bool hsearch_url(void *elementp, const void* searchkeyp){
 	char *ep = (char *)elementp;
 	char *skp = (char *)searchkeyp;
 	return strcmp(ep, skp) == 0;
 }
 
+bool qsearch_url(void *elementp,const void* keyp){
+	char *ep = (char *)webpage_getURL((webpage_t*)elementp);
+	char *skp = (char *)webpage_getURL((webpage_t *)keyp);
+	return strcmp(ep, skp) == 0;
+}
 
-void get_url(int depth, webpage_t *page, queue_t *url_queue){
+bool url_search_and_hput(webpage_t *page, hashtable_t *hash){
+	char *url = webpage_getURL(page);
+	long url_len = strlen(url);
+	if(hsearch(hash, hsearch_url, url, url_len) == NULL){
+	  hput(hash, url, url, url_len);
+		//printf("> added in hash!\n");
+		return false;
+	}else{
+		//printf("> already existed in hash\n");
+		return true;
+	}
+}
+
+void get_url(webpage_t *cur_page, queue_t *url_queue, hashtable_t *hash){
 	//get all the urls in the same layer
 	int pos = 0;
-	char *result;
-		
-	while((pos = webpage_getNextURL(page, pos, &result)) > 0){
-				//check whether the URL is internal
+	char *result;   //store urls crawled in current page
+	int cur_depth = webpage_getDepth(cur_page);
+	int next_depth = cur_depth+1;
+	void (*fn1)(queue_t *) = print_element;
+	bool (*fn2)(void *elementp, const void* searchkeyp) = qsearch_url;
+	printf("current page = %p\n",(void*)cur_page);
+        
+	if(webpage_fetch(cur_page)){
+
+		//check current page in hash
+		url_search_and_hput(cur_page, hash);
+					
+		while((pos = webpage_getNextURL(cur_page, pos, &result)) > 0){
+				
 				if(IsInternalURL(result)){
-					printf("Found URL (internal): %s\n",result);
+					printf("Depth %d: Found URL (internal): %s\n",next_depth, result);
 					//put URL in to the queue
-					webpage_t *new_page;
-					new_page = webpage_new(result, depth, NULL);
-					qput(url_queue, new_page);
+					webpage_t *new_page = webpage_new(result, next_depth, NULL);
+					if( qsearch(url_queue, fn2, new_page) == false &&\
+							url_search_and_hput(new_page, hash) == false){
+						//printf("put in queue!\n");
+						qput(url_queue, new_page);
+					}else{
+						//			printf("> already in queue!\n");
+					}
+					//check whether current page is in hash table, if not put it in
+										
 				}else{
-					printf("Found URL (external): %s\n\n",result);
+					printf("Depth %d: Found URL (external): %s\n\n",next_depth, result);
 				}
-				//free the URL for each iteration
+
 				free(result);
+		}
+	}else{
+		exit(EXIT_FAILURE);
 	}
+
+	printf("---------- Queue of URLs ------------\n");
+	qapply(url_queue, fn1);
+	printf("-------------------------------------\n\n");
 }
 
 int main(void){
 
-	printf("Hello\n");
+	//setup up basic variables
 	char seed[] = "https://thayer.github.io/engs50/";
 	int depth = 0;
-	webpage_t *page = webpage_new(seed, depth, NULL);;
-	void (*fn1)(queue_t *) = print_element;
-	//void (*fn2)(queue_t *) = put_url_in_hash;
+	uint32_t hsize = 500;
+	hashtable_t *url_hash  = hopen(hsize);
+	queue_t *url_queue = qopen();
+	//int count = 0;
+	//int count_limit = 200;
 
-	//if seed page fails
-	if(page == NULL){
-		printf("webpage_new error!\n");
+	//initiate the seed page
+	webpage_t *seed_page = webpage_new(seed, depth, NULL);;
+	get_url(seed_page, url_queue, url_hash);   		
 
-	//if seed page initialization succeed, fetch the page
-	}else{
-		printf("webpage_new succeed!\n");
-		
-		if(webpage_fetch(page)){
-			printf("webpage_fetch succeed!\n");
-
-			//get all the URL in the seed page
-			queue_t *url_queue = qopen();
-			
-			//getting url in designated page and put them in url_queue
-			get_url(depth, page, url_queue);
-
-			//open a hash to store url
-			uint32_t hsize = 500;    //set the size of the hash table to 500
-			hashtable_t *hash  = hopen(hsize);
-			
-			printf("---------- Queue of URLs ------------\n");
-			qapply(url_queue, fn1);
-
-			printf("\n---------- Hash of URLs ------------\n");
-			depth +=1;
-			void *qp = qget(url_queue);
-			while(qp != NULL){
-
-				webpage_t *wqp = (webpage_t *) qp;
-				char *url = webpage_getURL(wqp);				
-				long  url_len = strlen(url);
-				printf("URL = %s\n", url);
- 				get_url(depth, wqp, url_queue);
-				if(hsearch(hash, search_url, url, url_len)==NULL){
-					printf("Adding URL to hash table!\n");
- 					hput(hash, url, url, url_len);
- 				}else{	
-					printf("URL existed!\n");
-				}
-
-				qp = qget(url_queue);
-				free(url);
-			}
-
-			printf("\n---------- Queue updates ------------\n");
-			qapply(url_queue, fn1);
-			qclose(url_queue);
-			
-		//if fetch failed, exit	
-		}else{
-			exit(EXIT_FAILURE);
-		}
+	//iterate through all the pages in the queue until it's empty
+	void *qp = qget(url_queue);
+	while(qp != NULL){
+		get_url(qp, url_queue, url_hash);
+		qp = qget(url_queue);
 	}
-
-
-	webpage_delete(page);
+	
+	//qclose(url_queue);
+	//hclose(url_hash);
+	webpage_delete(seed_page);
 	exit(EXIT_SUCCESS);
 }
 
