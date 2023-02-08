1ee6f0d9d2e54c1d494480e95ca1fbcbdddecb2d
erin-bae <123341708+erin-bae@users.noreply.github.com>
2023-02-08 13:55:23 -0500

    Update crawler.c

--- module4/crawler/crawler.c
+++ module4/crawler/crawler.c
@@ -27,9 +27,30 @@ void print_element(void *p){
 	//	free(url);
 }
 
-/*int32_t pagesave(webpage_t *cur_page, int id, char *dirname){
-	fetch
-	}*/
+
+int32_t pagesave(webpage_t *pagep, int id, char *dirname) {
+	FILE *fp;
+	char path[64];
+	char name[64];
+	char depth[64];
+	char html_len[64];
+	char *url = webpage_getURL(pagep);
+	char *html = webpage_getHTML(pagep);
+
+	strcpy(path, dirname);	
+	sprintf(name, "%d", id);
+	sprintf(depth, "%d", webpage_getDepth(pagep));
+	sprintf(html_len, "%d", webpage_getHTMLlen(pagep));
+	
+	fp = fopen(strcat(path,name), "w+");
+
+	fprintf(fp, "%s\n%s\n%s\n%s\n", url, depth, html_len, html);
+	
+	fclose(fp);
+	
+	return 0;
+}
+
 
 bool hsearch_url(void *elementp, const void* searchkeyp){
 	char *ep = (char *)elementp;
@@ -100,29 +121,6 @@ int get_url(webpage_t *cur_page, int max, queue_t *url_queue, hashtable_t *hash)
 	//webpage_delete(new_page);
 	webpage_delete(cur_page);
 	return cur_depth;
-}
-
-int32_t pagesave(webpage_t *pagep, int id, char *dirname) {
-	FILE *fp;
-	char path[64];
-	char name[64];
-	char depth[64];
-	char html_len[64];
-	char *url = webpage_getURL(pagep);
-	char *html = webpage_getHTML(pagep);
-
-	strcpy(path, dirname);	
-	sprintf(name, "%d", id);
-	sprintf(depth, "%d", webpage_getDepth(pagep));
-	sprintf(html_len, "%d", webpage_getHTMLlen(pagep));
-	
-	fp = fopen(strcat(path,name), "w+");
-
-	fprintf(fp, "%s\n%s\n%s\n%s\n", url, depth, html_len, html);
-	
-	fclose(fp);
-	
-	return 0;
 }
 
 
