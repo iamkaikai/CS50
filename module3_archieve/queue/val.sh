ced421a64b52fa91a4fca90a81a5df62e44268c7
YENKAI HUANG <iamkaikai@livemail.tw>
2023-02-12 16:21:03 -0500

    delete Luis' backup folders

--- module3_archieve/queue/val.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash                                                                                                      
-#file=$(find . -name 'test*[0-9]')                                                                                
-                                                                                                                 
-#valgrind --leak-check=full $file
-valgrind --leak-check=full test1
-valgrind --leak-check=full test2
-valgrind --leak-check=full test3
