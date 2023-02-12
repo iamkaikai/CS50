9edfee7eb8722cb57ac859bbd451ac692ff93b24
YENKAI HUANG <yenkai.huang.gr@dartmouth.edu>
2023-02-12 16:25:09 -0500

    updating indexer.c

--- module5/indexer/indexer.c
+++ module5/indexer/indexer.c
@@ -13,7 +13,15 @@
 # include <stdlib.h>         
 # include <string.h>
 # include <stdbool.h>
+# include <webpage.h>
+# include <queue.h>
+# include <hash.h>
+# include <pageio.h>
 
 int main(void){
+	
 	printf("hello!\n");
+	webpage_t *page = pageload(1, "../pages/");
+	
+	int c = webpage_getNextWord(page, int pos, char **word);
 }
