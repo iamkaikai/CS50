6d1a89c59c2cdf802c77cd449f38a48c5d54f8f3
YENKAI HUANG <yenkai.huang.gr@dartmouth.edu>
2023-02-12 16:04:43 -0500

    add indexer

---
+++ module5/indexer/indexer.c
@@ -0,0 +1,19 @@
+/* indexer.c --- 
+ * 
+ * 
+ * Author: Yenkai Huang, Luis, Erin
+ * Created: Sun Feb 12 15:55:17 2023 (-0500)
+ * Version: 1.0
+ * 
+ * Description: indexer for tse 
+ * 
+ */
+
+# include <stdio.h>
+# include <stdlib.h>         
+# include <string.h>
+# include <stdbool.h>
+
+int main(void){
+	printf("hello!\n");
+}
