e4ed409d02b6fd30a8d491cbd700c2fbcf63dbbf
YENKAI HUANG <yenkai.huang.gr@dartmouth.edu>
2023-02-22 14:28:09 -0500

    add free functions

--- module6/querier/query.c
+++ module6/querier/query.c
@@ -23,6 +23,34 @@
 
 #define MAXSIZE 100 
 
+void print_queue_element(void* p){
+    idCountPair_t *qp = (idCountPair_t *) p;
+    char *current_id = qp->id;
+	int current_count = qp->count;
+    if(current_id != NULL){
+		// printf("current_id = %s\n",current_id);
+        printf("id = %s; count = %d\n", current_id, current_count);
+    }else{
+        printf("word error!\n");
+	}
+}
+
+void print_hash_element(void *p){
+    wordCountPair_t *qp = (wordCountPair_t *) p;
+    char *current_word = qp->word;
+    int current_count = qp->count;
+    queue_t* current_queue = qp->page_queue;
+    if(current_word != NULL){
+        printf("word = %s - total %d\n", current_word,current_count);
+        qapply(current_queue,print_queue_element);
+        printf("------------------\n");
+    }else if(current_word== NULL){
+        printf("word\n");
+    }else{
+			printf("word error!\n");
+    }
+}
+
 void print_query_array(void *p){
     char *qp = (char *) p;
     printf("%s ",qp);
@@ -34,6 +62,21 @@ void clean_query_array(queue_t *p){
         free(q);
         q = qget(p);
     }
+}
+
+void clean_idCountpair_queue(void *p){
+    idCountPair_t *qp = (idCountPair_t *) p;
+    char *id = qp->id;
+    free(id);
+}
+
+void removeWordAndQueue(void *p){
+    wordCountPair_t *qp = (wordCountPair_t *) p;
+    char *current_word = qp->word;
+	queue_t *current_queue = qp->page_queue;
+    qapply(current_queue, clean_idCountpair_queue);
+	qclose(current_queue);
+	free(current_word);
 }
 
 bool hsearch_word(void *elementp, const void* searchkeyp){
@@ -71,14 +114,21 @@ int main(void){
 
     printf("> ");
     scanf("%99[^\n]",input);
-    token = strtok(input, delimiter); 
     queryArray = qopen();
 
     while (input != NULL){
-        if (feof(stdin)) {
+       
+        
+        if (scanf("%99[^\n]", input) == EOF) {
             printf("\n");
+            qclose(queryArray);
+            happly(index_hash, removeWordAndQueue);
+            hclose(index_hash);
             return 0;
         }
+        
+         token = strtok(input, delimiter); 
+
 	    while(token != NULL){
             for(int i=0; i< strlen(token) && strcmp(&token[i],"\n"); i++) {
 			    if (!isalpha(token[i])) {
@@ -103,7 +153,7 @@ int main(void){
         //clean the queryArray 
         clean_query_array(queryArray);
         qclose(queryArray);
-        while ((c = getchar()) != '\n' && c != EOF);
+        while ((c = getchar()) != '\n');
         invalid = 0;
         memset(input, 0, MAXSIZE*sizeof(char*));
         
