0a65e0feea4728cb4fb9d0a543851b5d7ef0c08b
YENKAI HUANG <yenkai.huang.gr@dartmouth.edu>
2023-02-21 19:26:37 -0500

    fixed issue with step2 and step1

--- module6/querier/query.c
+++ module6/querier/query.c
@@ -22,33 +22,17 @@
 #include <indexio.h>
 
 #define MAXSIZE 100 
-char *word_for_search;  //store value for hsearch cmp
 
-void print_queue_element(void* p){
-    idCountPair_t *qp = (idCountPair_t *) p;
-    char *current_id = qp->id;
-	int current_count = qp->count;
-    if(current_id != NULL){
-		// printf("current_id = %s\n",current_id);
-        printf("id = %s; count = %d\n", current_id, current_count);
-    }else{
-        printf("word error!\n");
-	}
+void print_query_array(void *p){
+    char *qp = (char *) p;
+    printf("%s ",qp);
 }
 
-void print_hash_element(void *p){
-    wordCountPair_t *qp = (wordCountPair_t *) p;
-    char *current_word = qp->word;
-    int current_count = qp->count;
-    // queue_t* current_queue = qp->page_queue;
-    // printf("current_word = '%s'\n",current_word);
-    if(current_word != NULL && strcmp(current_word,word_for_search) ==0){
-        printf("%s: %d ", current_word,current_count);
-        // qapply(current_queue,print_queue_element);
-    }else if(current_word== NULL){
-        printf("word\n");
-    }else{
-		// printf("word error!\n");
+void clean_query_array(queue_t *p){
+    void *q = qget(p);
+    while(( q != NULL)){
+        free(q);
+        q = qget(p);
     }
 }
 
@@ -59,63 +43,77 @@ bool hsearch_word(void *elementp, const void* searchkeyp){
     return strcmp(ep, skp) == 0;
 }
 
-int main(void){
+void query_search_in_index(queue_t *p, hashtable_t *h){
+    char *s = qget(p);
+    int and, len, count;
+    while(s != NULL){
+        and = strcmp(s, "and");
+        len = strlen(s);
+        void *search = hsearch(h, hsearch_word, s, strlen(s));
+        if( search != NULL && and !=0 && len >2){
+            wordCountPair_t* wcp = (wordCountPair_t*)search;
+            count = wcp->count;
+            printf("%s: %d ", s, count);
+        }
+        s = qget(p);    //keep interating all the words in queryArray
+    }
+}
 
-    char input[MAXSIZE];
-    char* token; 
-    char* printArray[MAXSIZE];
-    char delimiter[]=" \t\n"; 
-    word_for_search = (char*)malloc(sizeof(char)*100);
-    printf("> ");
-    fgets(input,99,stdin);                          //input    
-    token = strtok(input, delimiter);               //split the first word from input
-    int qry_count = 0;                              //count how many words of input are there
+int main(void){
     hashtable_t *index_hash = hopen(999);           //hash for index
     indexload("../pages/", "index",index_hash);     //load index
-    
+    char input[MAXSIZE];
+    char* token; 
+    char delimiter[]=" \t\n"; 
+    queue_t *queryArray;
+    int c;
+    int invalid = 0;
+
+    printf("> ");
+    scanf("%99[^\n]",input);
+    token = strtok(input, delimiter); 
+    queryArray = qopen();
+
     while (input != NULL){
-        int invalid = 0;                            //keep track of invalid input
         if (feof(stdin)) {
             printf("\n");
             return 0;
         }
-	
-        while(token != NULL){
+	    while(token != NULL){
             for(int i=0; i< strlen(token) && strcmp(&token[i],"\n"); i++) {
 			    if (!isalpha(token[i])) {
-                    printf("[invalid query]\n");
+				    printf("[invalid query]\n");
 				    invalid = 1;
                     break;
 			    }
-                token[i] = tolower(token[i]);
+			    token[i] = tolower(token[i]);
 		    }
-            // printf("token = '%s'\n",token);
-            printArray[qry_count] = token; 
-            token = strtok(NULL, " \n"); 
-            qry_count++;
+            char *s = malloc(sizeof(char)*(strlen(token) + 1));
+            strcpy(s, token);
+		    qput(queryArray,s);
+            token = strtok(NULL, " ");
 		}
-    
         if(invalid != 1){
-            for(int j=0; j<qry_count; j++){
-                if(strlen(printArray[j])>2 && strcmp(printArray[j], "and") != 0){
-                    // printf("%s ", printArray[j]);           //for step1 grading
-                    if(hsearch(index_hash,hsearch_word,printArray[j],strlen(printArray[j]))){
-                        // printf("word '%s' found!\n", printArray[j]);
-                        word_for_search = printArray[j];
-                        // printf("word_for_search = '%s' \n", word_for_search);
-                        happly(index_hash,print_hash_element);
-                    }           
-                } 
-            }
+            printf("query keyword: ");
+            qapply(queryArray,print_query_array);
+            printf("\n");
+            query_search_in_index(queryArray, index_hash);
         }
-
-        // reset printArray and start a new line for a new input
-        memset(printArray, 0, MAXSIZE*sizeof(char*));
-        qry_count = 0;
-		printf("\n> ");
-   		fgets(input,99,stdin); 
-  	 	token = strtok(input, delimiter); 
+        
+        //clean the queryArray 
+        clean_query_array(queryArray);
+        qclose(queryArray);
+        while ((c = getchar()) != '\n' && c != EOF);
+        invalid = 0;
+        memset(input, 0, MAXSIZE*sizeof(char*));
+        
+        //start a new input
+        printf("\n> ");
+   		scanf("%99[^\n]",input); 
+  	 	token = strtok(input, delimiter);
+        queryArray = qopen(); 
 	}
-    free(word_for_search);
+    qclose(queryArray);
+    hclose(index_hash);
     return 0;
 }
